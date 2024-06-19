7ec16dcfe0936129ab822652f9c688eb9661442a
amorris683 <amorris.us@gmail.com>
2024-06-19 10:31:05 -0700

    implement llamaindex

--- code/ModularTests/VectorSearch.py
+++ code/ModularTests/VectorSearch.py
@@ -2,20 +2,21 @@ from sentence_transformers import SentenceTransformer, util
 import faiss
 import numpy as np
 import chromadb
+from llama_index import GPTSimpleVectorIndex, Document, Query
 
 class FAISS:
     def __init__(self, extracted_texts, model_name='paraphrase-MiniLM-L6-v2'):
         self.model = SentenceTransformer(model_name)
         self.texts = list(extracted_texts.values())
         self.embeddings = self.model.encode(self.texts, convert_to_tensor=True)
-        self.embeddings_np = self.embeddings.cuda().numpy()
+        self.embeddings_np = self.embeddings.cpu().numpy()
         
         dimension = self.embeddings_np.shape[1]
         self.index = faiss.IndexFlatL2(dimension)
         self.index.add(self.embeddings_np)
         
     def search(self, user_prompt, similarity_threshold=0.7):
-        query_embedding = self.model.encode([user_prompt], convert_to_tensor=True).cuda().numpy()
+        query_embedding = self.model.encode([user_prompt], convert_to_tensor=True).cpu().numpy()
         D, I = self.index.search(query_embedding, len(self.texts))
         
         # Convert distances to similarities
@@ -33,7 +34,7 @@ class ChromaVectorSearch:
         self.model = SentenceTransformer(model_name)
         self.texts = list(extracted_texts.values())
         self.embeddings = self.model.encode(self.texts, convert_to_tensor=True)
-        self.embeddings_np = self.embeddings.cuda().numpy()
+        self.embeddings_np = self.embeddings.cpu().numpy()
 
         self.client = chromadb.Client()
         self.collection = self.client.create_collection("documents")
@@ -46,7 +47,7 @@ class ChromaVectorSearch:
             )
 
     def search(self, user_prompt, similarity_threshold=0.7):
-        query_embedding = self.model.encode([user_prompt], convert_to_tensor=True).cuda().numpy().tolist()[0]
+        query_embedding = self.model.encode([user_prompt], convert_to_tensor=True).cpu().numpy().tolist()[0]
 
         # Search documents
         results = self.collection.query(
@@ -64,3 +65,33 @@ class ChromaVectorSearch:
                 relevant_similarities.append(similarity)
 
         return relevant_texts, relevant_similarities
+
+    class LlamaIndex:
+        def __init__(self, extracted_texts, model_name='paraphrase-MiniLM-L6-v2'):
+            self.model = SentenceTransformer(model_name)
+            self.texts = list(extracted_texts.values())
+            self.embeddings = self.model.encode(self.texts, convert_to_tensor=True)
+            self.embeddings_np = self.embeddings.cpu().numpy()
+
+            # convert docs to list
+            self.documents = [Document(text=text) for text in self.texts]
+
+            # create llamaindex
+            self.index = GPTSimpleVectorIndex(documents=self.documents, embedding_model=self.model)
+
+        def search(self, user_prompt, similarity_threshold=0.7):
+            query_embedding = self.model.encode([user_prompt], convert_to_tensor=True).cpu().numpy().tolist()[0]
+
+            # perform search
+            query = Query(text=user_prompt, embedding=query_embedding)
+            results = self.index.query(query, top_k=len(self.texts))
+
+            relevant_texts = []
+            relevant_similarities = []
+            for result in results:
+                similarity = result.similarity
+                if similarity >= similarity_threshold:
+                    relevant_texts.append(result.document.text)
+                    relevant_similarities.append(similarity)
+
+            return relevant_texts, relevant_similarities
