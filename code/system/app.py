4946e3cc295ed8e83372289380603c0a418d5af8
Diego Estuar <144483878+destuar@users.noreply.github.com>
2024-07-16 21:59:18 -0700

    Update app.py

--- code/system/app.py
+++ code/system/app.py
@@ -10,12 +10,12 @@ from langchain_core.output_parsers import StrOutputParser
 import time
 
 chla_dir = 'chla_vectorstore'
-embedding = SentenceTransformerEmbeddings(model_name='all-MiniLM-L6-v2')
+embedding = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
 chla_vectordb = Chroma(embedding_function=embedding, persist_directory=chla_dir)
 chla_retriever = chla_vectordb.as_retriever(search_kwargs={'k': 2})
 
 cdc_dir = 'cdc_vectorstore'
-embedding = SentenceTransformerEmbeddings(model_name='all-MiniLM-L6-v2')
+embedding = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
 cdc_vectordb = Chroma(embedding_function=embedding, persist_directory=cdc_dir)
 cdc_retriever = cdc_vectordb.as_retriever(search_kwargs={'k': 2})
 
